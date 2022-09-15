b035d4ccd4a12529db9e41507c6eca1879fee1dc
MadsJaeger <madshjaeger@gmail.com>
2022-09-15 16:23:39 +0200

    Added relation of order to order items and ensured autoassociation of basket and updating basket to order.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,18 @@
+# frozen_string_literal: true
+
+##
+# Registration of checked out order items per user
 class Order < ApplicationRecord
   belongs_to :user, optional: false
+  has_many :order_items, dependent: :restrict_with_error
+
+  before_validation do
+    associate_order_items
+  end
+
+  validates :order_items, length: { minimum: 1 }
+
+  def associate_order_items
+    self.order_items += OrderItem.basket.where(user: user)
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,27 +2,47 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   before :all do
-    @user = User.create!(
-      name:                  'Tester',
-      email:                 'test@email.here',
-      password:              '!1SecurePwd',
-      password_confirmation: '!1SecurePwd',
-    )
+    @user = create(:user)
+    @items = create_list(:order_item, 10, user: @user)
+    @products = @items.map(&:product)
   end
 
   let :subject do
-    describe_class.new(
-      user: @user
-    )
+    build(:order, user: @user, item_count: 0)
   end
 
-  describe 'validations' do
-    it 'subject is valid' do
+
+  describe 'validation' do
+    it 'is valid' do
       expect(subject).to be_valid
+    end
+
+    it 'is invalid without any items' do
+      subject.user = build(:user)
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'basket' do
+    it 'has no items' do
+      expect(subject.order_items.length).to be 0
+    end
+
+    it 'associates basket before validation' do
+      subject.valid?
+      expect(subject.order_items.length).to be 10
+    end
+
+    it 'updated items with order_id on save' do
+      subject.save
+      expect(subject.order_items.map(&:order_id_in_database)).to all( be subject.id )
     end
   end
 
   after :all do
+    @items.each(&:destroy!)
+    @products.map(&:prices).flatten.each(&:destroy!)
+    @products.each(&:reload).each(&:destroy!)
     @user.destroy!
   end
 end
