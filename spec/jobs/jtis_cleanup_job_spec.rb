3cc624c15920b15e100434b11241ac7bd291a4dc
MadsJaeger <madshjaeger@gmail.com>
2022-09-20 12:12:55 +0200

    Corrected bad database ussage

--- spec/jobs/jtis_cleanup_job_spec.rb
+++ spec/jobs/jtis_cleanup_job_spec.rb
@@ -3,17 +3,18 @@ require 'rails_helper'
 RSpec.describe JtiCleanupJob, type: :job do
   describe '#perform' do
     before :each do
-      create(:jti, exp: DateTime.now + 5.seconds)
-      build(:jti, exp: DateTime.now - 5.seconds).save(validate: false)
+      @user = create(:user)
+      create(:jti, user: @user, exp: DateTime.now + 5.seconds)
+      build(:jti, user: @user, exp: DateTime.now - 5.seconds).save(validate: false)
       subject.perform
     end
 
     it 'persist current jtis' do
-      expect(Jti.count).to be 1
+      expect(Jti.where(user: @user).count).to be 1
     end
 
     it 'deletes expired jtis' do
-      expect(Jti.expired.count).to be 0
+      expect(Jti.where(user: @user).expired.count).to be 0
     end
   end
 end
